d8071a68d27f4e03d8dcea960662b07bc99613c8
Davoud Yousuf <128808526+davoudcodes@users.noreply.github.com>
2026-02-21 17:00:19 +0530

    Added languageOptions Test

--- pages/home_page.py
+++ pages/home_page.py
@@ -6,17 +6,25 @@ class HomePage:
         self.page = page
         self.signin_link = page.locator('#nav-link-accountList a')
         self.app_logo = page.locator('#nav-logo a')
+        self.choose_language_element = page.locator("span[class='icp-nav-link-inner'] span[class='nav-line-2']")
+        self.get_language_option_element =  page.locator("#nav-flyout-icp  ul[role='list'] > li")
 
     def get_homepage_title(self):
         title = self.page.title()
         return title
 
-    def click_sigin_link(self):
+    def click_signin_link(self):
         try:
             self.signin_link.click()
         except Exception as e:
             print(f" Exception while clicking 'Sign in': {e}")
             raise
+    def get_language_options(self):
+        self.choose_language_element.hover()
+        self.page.wait_for_timeout(5000)
+        return self.get_language_option_element
+
+
 
 
 
--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -4,19 +4,28 @@ from pages.home_page import HomePage
 from pages.signin_page import signinPage
 
 
-@pytest.mark.sanity
+
 def test_homepage_logo(page):
     homepage = HomePage(page)
     page.wait_for_timeout(5000)
     expect(homepage.app_logo).to_be_visible()
 
-@pytest.mark.sanity
+
 def test_signin_link(page):
     homepage = HomePage(page)
     signin_page = signinPage(page)
-    homepage.click_sigin_link()
+    homepage.click_signin_link()
     page.wait_for_timeout(5000)
     signinpage_text_element = signin_page.get_signin_page_text_element()
     expect(signinpage_text_element).to_be_visible(timeout=5000)
     expect(signinpage_text_element).to_have_text("Sign in or create account")
+
+@pytest.mark.sanity
+def test_language_options(page):
+    homepage = HomePage(page)
+    page.wait_for_timeout(5000)
+    language_options = homepage.get_language_options().all()
+    for language_option in language_options:
+        print(language_option.inner_text())
+        expect(language_option).to_be_visible()
 
